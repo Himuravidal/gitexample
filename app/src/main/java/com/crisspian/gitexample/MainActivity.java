4f5abadcfe2f5eb2ddfb7c3bf83cb050302e6704
David Galleguillos <galleguillos.david@gmail.comglobal user.email galleguillos.david@gmail.com>
2020-08-10 23:13:19 -0400

    Version 3

--- app/src/main/java/com/crisspian/gitexample/MainActivity.java
+++ app/src/main/java/com/crisspian/gitexample/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String name = "David Galleguillos";
+        String name = "este es mi nombre";
+        String name1 = "David Galleguillos";
     }
 }
